c3d997f446032fc7d9e1188d1c2825586faefb29
Tarek Saier <tareksaier@gmail.com>
2013-09-12 21:20:47 +0200

    dev, dev ...

--- kanjiplot.py
+++ kanjiplot.py
@@ -4,10 +4,17 @@ import sqlite3
 import unicodedata
 import datetime
 
+def addUp():
+	sum = 0
+	for d in data_points:
+		sum = sum + data_points[d]
+	return sum
+
 conn = sqlite3.connect('collection.anki2')
 c = conn.cursor()
-notes = 0
+dates = []
 kanji = []
+data_points = dict()
 for row in c.execute('SELECT id, flds FROM notes WHERE mid IS 1367422637589 ORDER BY id'):
 	timestamp = row[0]
 	date = datetime.datetime.fromtimestamp(timestamp/1000).strftime("%y%m%d")
@@ -18,8 +25,13 @@ for row in c.execute('SELECT id, flds FROM notes WHERE mid IS 1367422637589 ORDE
 			if(unicodedata.name(char).find('CJK UNIFIED IDEOGRAPH') >= 0):
 				if(not char in kanji):
 					kanji.append(char)
-					notes = notes + 1
+					if(not date in dates):
+						print(addUp())
+						dates.append(date)
+						data_points[date] = 0
+					data_points[date] += 1
 		except ValueError:
 			pass
-
-print(notes)
+for d in data_points:
+	#print(str(d) + ' ' + str(data_points[d]))
+	pass
