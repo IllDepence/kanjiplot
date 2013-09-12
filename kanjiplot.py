e53eab6ce2bfdcab9a67f7acdd7eb0c994dc4368
Tarek Saier <tareksaier@gmail.com>
2013-09-12 21:31:50 +0200

    pythonscript works, next: plotting

--- kanjiplot.py
+++ kanjiplot.py
@@ -4,17 +4,12 @@ import sqlite3
 import unicodedata
 import datetime
 
-def addUp():
-	sum = 0
-	for d in data_points:
-		sum = sum + data_points[d]
-	return sum
-
 conn = sqlite3.connect('collection.anki2')
 c = conn.cursor()
 dates = []
 kanji = []
 data_points = dict()
+total = 0
 for row in c.execute('SELECT id, flds FROM notes WHERE mid IS 1367422637589 ORDER BY id'):
 	timestamp = row[0]
 	date = datetime.datetime.fromtimestamp(timestamp/1000).strftime("%y%m%d")
@@ -24,14 +19,14 @@ for row in c.execute('SELECT id, flds FROM notes WHERE mid IS 1367422637589 ORDE
 		try:
 			if(unicodedata.name(char).find('CJK UNIFIED IDEOGRAPH') >= 0):
 				if(not char in kanji):
+					total += 1
 					kanji.append(char)
 					if(not date in dates):
-						print(addUp())
 						dates.append(date)
-						data_points[date] = 0
-					data_points[date] += 1
+					data_points[date] = total
 		except ValueError:
 			pass
-for d in data_points:
-	#print(str(d) + ' ' + str(data_points[d]))
-	pass
+
+f = open('kanji.dat', 'w')
+for d in dates:
+	f.write(str(d) + ' ' + str(data_points[d]) + '\n')
